ec397e10bb374aa2fcb9c214af6dc4775a3c076d
snow915 <snow915@protonmail.com>
2020-08-12 16:58:30 -0500

    add: TouchableWithoutFeedback in web image

--- App.js
+++ App.js
@@ -1,36 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Platform, Image } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Platform,
+    Image,
+    TouchableWithoutFeedback
+} from 'react-native';
 
 export default function App() {
-  const textPressed = () => console.log("Text pressed");
-  return (
-    <SafeAreaView style={[styles.container, styles.droidSafeArea]}>
-      <Text>Image from assets!</Text>
-      <Image source={require('./assets/icon.png')} />
-      <Text>Image from web</Text>
-      <Image
-          fadeDuration={2000}
-          source={{
-          width: 200,
-          height: 300,
-          uri : 'https://picsum.photos/200/300'
-        }} />
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+    const textPressed = () => console.log("Text pressed");
+    return (
+        <SafeAreaView style={[styles.container, styles.droidSafeArea]}>
+            <Text>Image from assets!</Text>
+            <Image source={require('./assets/icon.png')}/>
+            <Text>Image from web</Text>
+            <TouchableWithoutFeedback onPress={() => console.log('Image pressed')}>
+                <Image
+                    fadeDuration={2000}
+                    source={{
+                        width: 200,
+                        height: 300,
+                        uri: 'https://picsum.photos/200/300'
+                    }}
+                />
+            </TouchableWithoutFeedback>
+            <StatusBar style="auto"/>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 
-  droidSafeArea : {
-    flex: 1,
-    paddingTop: Platform.OS === 'android' ? 25 : 0
-  }
+    droidSafeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 25 : 0
+    }
 });
