49e71300f97910d1f46b155eaeebc8cce89475e0
snow915 <snow915@protonmail.com>
2020-08-13 19:51:54 -0500

    add: dimensions with useDimensions and useDeviceOrientation, when the device is in landscape mode, the view size is changed

--- App.js
+++ App.js
@@ -1,45 +1,30 @@
 import React from 'react';
 import {
     StyleSheet,
-    Text,
     View,
     SafeAreaView,
     Platform,
-    Button,
-    Alert,
-    StatusBar
+    StatusBar,
+    Dimensions
 } from 'react-native';
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
 
-    const showAlert = () => {
-        Alert.alert('My title', 'My message', [
-            { text: 'Yes', onPress: () => console.log('Yes') },
-            { text: 'No', onPress: () => console.log('No') }
-        ]);
-    }
-
-    // For android not working, only for iOS
-    const showPrompt = () => {
-        Alert.prompt('Personal data', 'Whats your name?', (text)=> console.log(text));
-    }
+    console.log(useDimensions())
+    console.log(useDeviceOrientation())
+    const { landscape } = useDeviceOrientation();
 
     return (
         <SafeAreaView style={styles.droidSafeArea}>
             <View style={styles.container}>
-                <Text>Image from assets!</Text>
-                <Button
-                    title='Show Alert'
-                    onPress={ showAlert }
-                />
-                <Button
-                    title='Show Prompt'
-                    color='orange'
-                    onPress={ showPrompt }
-                />
-                <StatusBar style="auto"/>
+                <View style={{
+                    backgroundColor: 'dodgerblue',
+                    width: '100%',
+                    height: landscape ? '100%' : '25%'
+                }}>
+                </View>
             </View>
-
         </SafeAreaView>
     );
 }
@@ -48,8 +33,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        //alignItems: 'center',
+        //justifyContent: 'center',
     },
 
     droidSafeArea: {
